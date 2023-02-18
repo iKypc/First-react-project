0411bd06894f4f1e56ae006e70bd71a623a182b3
Митяй <kotrashchenko@gmail.com>
2023-02-18 04:32:59 +0200

    add ui button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import React from "react";
-import PostItem from "./components/PostItem";
+import React, {useState} from "react";
+// import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
 import './styles/app.css';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript', body: 'Description'},
+    {id: 2, title: 'Javascript 2', body: 'Description'},
+    {id: 3, title: 'Javascript 3', body: 'Description'},
+    {id: 4, title: 'Javascript 4', body: 'Description'},
+    {id: 5, title: 'Javascript 5', body: 'Description'}
+  ])
+
+
   return (
     <div className="App">
-
-      <PostItem post={{id: 1, title: 'Javascript', body: 'Description'}} />
-      
+      <form action="">
+        <input type="text" placeholder="Название поста"/>
+        <input type="text" placeholder="Описание поста"/>
+        <MyButton disabled>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title='JS:'/>
     </div>
   );
 }
