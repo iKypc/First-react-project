5c0982560af6e7e734db48337dfa867cdda09369
Митяй <kotrashchenko@gmail.com>
2023-02-18 03:02:18 +0200

    create first component

---
+++ src/components/Counter.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+  const [count, setCount] = useState(0)
+
+  function inc() {
+    setCount(count + 1);
+  }
+  function dec() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div>
+    <h1>{count}</h1>    
+      <button onClick={inc}>+</button>
+      <button onClick={dec}>-</button>
+      </div>
+  )
+}
+
+export default Counter
